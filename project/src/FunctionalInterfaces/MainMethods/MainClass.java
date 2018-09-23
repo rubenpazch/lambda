e6912d6ecdd76a77a3d1993f49044fbf81d5b1dc
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2018-09-23 01:52:49 -0500

    asdasd

---
+++ project/src/FunctionalInterfaces/MainMethods/MainClass.java
@@ -0,0 +1,10 @@
+package FunctionalInterfaces.MainMethods;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ project/src/FunctionalInterfaces/StaticMethods/Library.java
@@ -0,0 +1,5 @@
+package FunctionalInterfaces.StaticMethods;
+
+public class Library {
+
+}
